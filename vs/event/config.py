ffb9d66ccadc97e2144b6b995e7a859e70462ceb
thet <johannes@raggam.co.at>
2009-12-10 20:21:40 +0000

    product installation moved to GS, metadata.xml

--- vs/event/config.py
+++ vs/event/config.py
@@ -2,10 +2,7 @@
 
 ################################################################
 # vs.event - published under the GPL 2
-# Authors: Andreas Jung, Veit Schiele, Anne Walther 
+# Authors: Andreas Jung, Veit Schiele, Anne Walther
 ################################################################
 
 PROJECTNAME = "vs.event"
-DEPENDENCIES=['dateable.chronos',
-              'DataGridField']
-
