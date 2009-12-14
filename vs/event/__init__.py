faee08e2ccde7e191ed2a907aad5dc9d27e801ec
thet <johannes@raggam.co.at>
2009-12-14 16:59:26 +0000

    foo

--- vs/event/__init__.py
+++ vs/event/__init__.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 ################################################################
 # vs.event - published under the GPL 2
-# Authors: Andreas Jung, Veit Schiele, Anne Walther 
+# Authors: Andreas Jung, Veit Schiele, Anne Walther
 ################################################################
-
-from zope.i18nmessageid import MessageFactory
 from config import PROJECTNAME
+
+try:
+    from Products.LinguaPlone import public as atapi
+except ImportError:
+    from Products.Archetypes import atapi
 
 from Products.CMFCore import utils
-from Products.Archetypes import atapi
 
+from AccessControl import ModuleSecurityInfo
+from zope.i18nmessageid import MessageFactory
 MessageFactory = MessageFactory(PROJECTNAME)
-from config import PROJECTNAME
+ModuleSecurityInfo('vs.event').declarePublic('MessageFactory')
+
 from logging import getLogger
-log = getLogger(">>>>>>>>")
+logger = getLogger(PROJECTNAME)
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
@@ -26,12 +31,14 @@ def initialize(context):
     for type in content_types:
         ADD_PERMISSIONS[type.portal_type] = """Add %s""" %type.portal_type
 
-    for atype, constructor in zip(content_types, constructors):
-        utils.ContentInit("%s: %s" % (PROJECTNAME, atype.portal_type),
-        content_types      = (atype,),
-        permission         = ADD_PERMISSIONS[atype.portal_type],
-        extra_constructors = (constructor,),
-        ).initialize(context)
+    allTypes = zip(content_types, constructors)
+    for atype, constructor in allTypes:
+        utils.ContentInit(
+            "%s: %s" % (PROJECTNAME, atype.portal_type),
+            content_types      = (atype,),
+            permission         = ADD_PERMISSIONS[atype.portal_type],
+            extra_constructors = (constructor,),
+            ).initialize(context)
 
 
 #from Products.CMFCore.permissions import setDefaultRoles
