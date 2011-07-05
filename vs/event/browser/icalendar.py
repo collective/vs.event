ab862f7597a189136b54872286864804df13a5a1
claytron <me@claytron.com>
2011-07-05 14:28:52 +0000

    use the request from the browser view since the request on the context errors out

--- vs/event/browser/icalendar.py
+++ vs/event/browser/icalendar.py
@@ -48,11 +48,11 @@ class iCalendarView(BrowserView):
         write('END:VCALENDAR')
 
         body = '\n'.join(result)
-        setHeader = self.context.request.response.setHeader
+        setHeader = self.request.response.setHeader
         setHeader('Content-Length', len(body))
         setHeader('Content-Type', 'text/x-vcalendar')
         setHeader('Content-Disposition', 'attachment; filename=plone-cal.ics')
-        return self.context.request.response.write(body)
+        return self.request.response.write(body)
 
     def icalendar_export_event(self):
         """ iCal export for a single event (including subevents) """
